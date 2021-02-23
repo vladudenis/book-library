05fedec15962be47901e4f8d88619da13a6a76c2
Denis V <vladudenis09@gmail.com>
2021-02-23 10:06:28 +0100

    This is pretty much it, but I'll come back and make it all look cooler and add more functionality when I feel inspired.

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 let myLibrary = [];
 
-function Book(title, author, pages, read){
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function(){
+class Book {
+    constructor(
+      title = "Unknown",
+      author = "Unknown",
+      pagecount = "0",
+      read = "false"
+    ) {
+      this.title = title;
+      this.author = author;
+      this.pagecount = pagecount;
+      this.read = read;
+      this.info = function(){
         return title + " by " + author + ", " + pages + " pages, " + (read ? "have read" : "haven't read yet");
+      }
     }
 }
 
@@ -17,6 +24,19 @@ function addBookToLibrary(newBook){
         myLibrary.push(newBook);
         return true;
     }
+}
+
+function removeBookFromLibrary(title){
+    myLibrary = myLibrary.filter((book) => book.title != title);
+}
+
+function whichBook(bookTitle){
+    for(let book of myLibrary){
+        if(book.title === bookTitle){
+            return book;
+        }
+    }
+    return null;
 }
 
 //new book button, modal and form
@@ -44,7 +64,7 @@ window.addEventListener("keydown", (e) => {
     if (e.key === "Escape") closeModal();
   });
 
-function addBook(e){
+function addBook(e){  
     e.preventDefault();
     if(addBookToLibrary(getBook())){
         updateBooksGrid();
@@ -55,44 +75,63 @@ function addBook(e){
 }
 
 function getBook(){
-    const title = document.querySelector("#title");
-    const author = document.querySelector("#author");
-    const pagecount = document.querySelector("#pagecount");
-    const read = document.querySelector("#read");
-
+    const title = form.querySelector("#title").value;
+    const author = form.querySelector("#author").value;
+    const pagecount = form.querySelector("#pagecount").value;
+    const read = form.querySelector("#read").checked;
     return new Book(title, author, pagecount, read);
 }
 
 const bookshelf = document.querySelector(".bookshelf");
+bookshelf.addEventListener("click", checkGridInput);
+
+function checkGridInput(e){
+    if(e.target.classList.contains("remove-btn")){
+        removeBookFromLibrary(e.target.parentNode.firstChild.innerHTML);
+        e.target.parentNode.parentNode.removeChild(e.target.parentNode);
+    }else if(e.target.classList.contains("read-btn")){
+        if(e.target.innerHTML == "Read"){
+            whichBook(e.target.parentNode.firstChild.innerHTML).read = false;
+            e.target.innerHTML = "Not Read";
+            //add one color and remove the other by classList
+        }else{
+            whichBook(e.target.parentNode.firstChild.innerHTML).read = true;
+            e.target.innerHTML = "Read";
+            //same as before
+        }
+    }
+}
 
 function updateBooksGrid() {
     resetGrid();
     for (let element of myLibrary) {
       createBook(element);
     }
-  }
+}
   
   function resetGrid() {
     bookshelf.innerHTML = "";
-  }
+}
 
 function createBook(book){
     const myBook = document.createElement("div");
     const title = document.createElement("h3");
     const author = document.createElement("h3");
     const pagecount = document.createElement("h3");
-    const read = document.createElement("button"); //change to slider
+    const read = document.createElement("button");
     const remove = document.createElement("button");
 
     myBook.classList.add("bookshelf__book");
     title.classList.add("bookshelf__book-text");
     author.classList.add("bookshelf__book-text");
     pagecount.classList.add("bookshelf__book-text");
-    read.classList.add("button"); //change to slider
+    read.classList.add("button");
+    read.classList.add("read-btn");
     remove.classList.add("button");
+    remove.classList.add("remove-btn");
 
     title.textContent = book.title;
-    author.textCOntent = book.author;
+    author.textContent = book.author;
     pagecount.textContent = `${book.pagecount} pages`;
     remove.textContent = "Remove";
     if (book.read){
@@ -108,5 +147,5 @@ function createBook(book){
     myBook.appendChild(pagecount);
     myBook.appendChild(read);
     myBook.appendChild(remove);
-    bookshelf.appendChild(book);
+    bookshelf.appendChild(myBook);
 }
